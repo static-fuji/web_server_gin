b06eb97e1630202c149d4e9435612481766d2eac
static-fuji <jyun1228haru@icloud.com>
2024-07-22 16:21:47 +0900

    make handler returns selected item

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func getAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 	router.Run("localhost:8080")
 }
@@ -44,4 +45,16 @@ func postAlbums(c *gin.Context) {
 	//スライスへ新しいアルバムを追加する
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
